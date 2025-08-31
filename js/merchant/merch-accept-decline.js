f9be3c89f568ee5f42fd7f139a9acf86a10c9599
Scholar909 <enehogbole909@gmail.com>
2025-08-31 20:29:19 +0100

    Update merch-accept-decline.js

--- js/merchant/merch-accept-decline.js
+++ js/merchant/merch-accept-decline.js
@@ -68,7 +68,7 @@ onAuthStateChanged(auth, (user) => {
 
   const q = query(collection(db, "orders"), where("assignedMerchantId", "==", user.uid));
   onSnapshot(q, (snapshot) => {
-    ordersList.innerHTML = `<p>No Order Available</p>` ;
+    ordersList.innerHTML = '';
     snapshot.forEach(docSnap => {
       const order = { id: docSnap.id, ...docSnap.data() };
       if (order.orderStatus === "pending") {
