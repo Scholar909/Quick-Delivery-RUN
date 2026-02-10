f8cc992a54a301cbfbbae4a17593276682da1e9d
Scholar909 <enehogbole909@gmail.com>
2026-02-10 19:39:23 +0100

    Update pending-orders.js

--- js/customer/pending-orders.js
+++ js/customer/pending-orders.js
@@ -6,8 +6,8 @@ import {
   orderBy,
   onSnapshot,
   serverTimestamp
-} from 'https://www.gstatic.com/firebasejs/11.8.1/firebase-firestore.js';
-import { onAuthStateChanged } from 'https://www.gstatic.com/firebasejs/11.8.1/firebase-auth.js';
+} from 'https://www.gstatic.com/firebasejs/12.9.0/firebase-firestore.js';
+import { onAuthStateChanged } from 'https://www.gstatic.com/firebasejs/12.9.0/firebase-auth.js';
 
 // Tabs & containers
 const pendingTab = document.getElementById('pending');
