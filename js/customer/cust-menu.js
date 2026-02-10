c16442d4b09df818de49353601a58cdb8a293921
Scholar909 <enehogbole909@gmail.com>
2026-02-10 19:36:38 +0100

    Update Firebase SDK version to 12.9.0

--- js/customer/cust-menu.js
+++ js/customer/cust-menu.js
@@ -14,8 +14,8 @@ import {
   serverTimestamp,
   Timestamp,
   deleteDoc
-} from 'https://www.gstatic.com/firebasejs/11.8.1/firebase-firestore.js';
-import { onAuthStateChanged } from "https://www.gstatic.com/firebasejs/11.8.1/firebase-auth.js";
+} from 'https://www.gstatic.com/firebasejs/12.9.0/firebase-firestore.js';
+import { onAuthStateChanged } from "https://www.gstatic.com/firebasejs/12.9.0/firebase-auth.js";
 
 onAuthStateChanged(auth, async (user) => {
   if (user) {
