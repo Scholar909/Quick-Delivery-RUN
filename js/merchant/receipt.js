ff2532c952139d2e60f3bf7cca89bed5b21f0934
Scholar909 <enehogbole909@gmail.com>
2026-02-10 19:49:49 +0100

    Update Firebase imports to version 12.9.0

--- js/merchant/receipt.js
+++ js/merchant/receipt.js
@@ -5,8 +5,8 @@ import {
   doc,
   onSnapshot,
   getDoc
-} from 'https://www.gstatic.com/firebasejs/11.8.1/firebase-firestore.js';
-import { onAuthStateChanged } from 'https://www.gstatic.com/firebasejs/11.8.1/firebase-auth.js';
+} from 'https://www.gstatic.com/firebasejs/12.9.0/firebase-firestore.js';
+import { onAuthStateChanged } from 'https://www.gstatic.com/firebasejs/12.9.0/firebase-auth.js';
 
 /* ---------- helpers ---------- */
 const $ = (id) => document.getElementById(id);
