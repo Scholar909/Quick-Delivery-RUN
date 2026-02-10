bd9c4bd58915cbc9611b4aed1907fe9e9b21a918
Scholar909 <enehogbole909@gmail.com>
2026-02-10 19:08:49 +0100

    Update Firebase Firestore version to 12.9.0

--- js/admin/ad-combo-rec.js
+++ js/admin/ad-combo-rec.js
@@ -8,7 +8,7 @@ import {
   getDoc,
   onSnapshot,
   setDoc
-} from 'https://www.gstatic.com/firebasejs/11.8.1/firebase-firestore.js';
+} from 'https://www.gstatic.com/firebasejs/12.9.0/firebase-firestore.js';
 
 document.addEventListener("DOMContentLoaded", () => {
   // Tabs
