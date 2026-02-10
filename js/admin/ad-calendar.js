eb2dc29441a81f16d127930fc7f7a9fbe9098350
Scholar909 <enehogbole909@gmail.com>
2026-02-10 19:08:04 +0100

    Update Firebase Firestore version to 12.9.0

--- js/admin/ad-calendar.js
+++ js/admin/ad-calendar.js
@@ -9,7 +9,7 @@ import {
   getDoc,
   setDoc,
   updateDoc
-} from 'https://www.gstatic.com/firebasejs/11.8.1/firebase-firestore.js';
+} from 'https://www.gstatic.com/firebasejs/12.9.0/firebase-firestore.js';
 
 document.addEventListener("DOMContentLoaded", () => {
   const merchantSelect = document.getElementById("merchant-select");
